7a1246c996b2acdccf68eb753df96773c0f1bb96
shenghongzhong <howdavidlearncoding@gmail.com>
2025-02-12 14:25:59 +0000

    Added all hashtable relevant functions

---
+++ src/hashtable/hashtable_iterate.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hashtable_iterate.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szhong <szhong@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 16:44:11 by szhong            #+#    #+#             */
+/*   Updated: 2024/12/19 16:44:12 by szhong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "hashtable.h"
+
+void hashmap_iterate(t_hashmap *table, void (*f)(const char *, const char *))
+{
+    size_t i;
+    t_hash_item *item;
+
+    if (!table || !f)
+        return;
+    
+    for (i = 0; i < table->size; i++)
+    {
+        item = table->items[i];
+        while (item)
+        {
+            f(item->key, item->value);
+            item = item->next;
+        }
+    }
+}
